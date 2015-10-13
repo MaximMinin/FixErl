b6b46d7e410d4e54f23530231c533f8984bb1e3a
MaximMinin <maxim.minin@gmx.de>
2015-10-13 10:37:56 +0200

    tables have to be created  by session supervisor.

--- src/fix_session_sup.erl
+++ src/fix_session_sup.erl
@@ -27,6 +27,7 @@
 %% ===================================================================
 
 start_link(#session_parameter{id = Id} = S) ->
+    fixerl_mnesia_utils:create_table(Id),
     supervisor:start_link({local, erlang:list_to_atom(
                                     lists:concat([Id, "_",?MODULE]))}, 
                             ?MODULE, [S]).
--- src/fixerl_root_sup.erl
+++ src/fixerl_root_sup.erl
@@ -52,9 +52,8 @@ start_link() ->
 %%      {ok, pid()} | {error, Reason}
 %%
 %% @end
-start_session(#session_parameter{id=Id}=S) ->
+start_session(#session_parameter{}=S) ->
     lager:info("Start session: ~p",[S]),
-    fixerl_mnesia_utils:create_table(Id),
     supervisor:start_child(?MODULE, [S]).
 
 %% --------------------------------------------------------------------
